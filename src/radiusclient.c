033ac763de759c88ec46e69379b0da3c8faf8a2d
sobomax <sobomax>
2004-10-17 18:25:27 +0000

    Add new switch `-s', which if specified enables special mode of operation. In this mode radiusclient reads set of attributes, sends then to server and vrites result as well as received attributes to the stdout. Empty string signals that input it complete and radiusclient should process with authentification or accounting.

--- src/radiusclient.c
+++ src/radiusclient.c
@@ -23,37 +23,41 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: radiusclient.c,v 1.2 2004/10/05 09:35:46 sobomax Exp $
+ * $Id: radiusclient.c,v 1.3 2004/10/17 18:25:27 sobomax Exp $
  */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 #include <radiusclient.h>
 
+int process(void *, VALUE_PAIR *, int, int);
+
 static void
 usage(void)
 {
 
-    fprintf(stderr, "usage: radiusclient [-a] [-f config_file] [-p nas_port] a1=v1 [a2=v2[...[aN=vN]...]]\n");
+    fprintf(stderr, "usage: radiusclient [-f config_file] [-p nas_port] [-s | [-a] a1=v1 [a2=v2[...[aN=vN]...]]]\n");
     exit(1);
 }
 
 int
 main(int argc, char **argv)
 {
-    int i, nas_port, ch, acct;
+    int i, nas_port, ch, acct, server, ecount, firstline, len, theend;
     void *rh;
-    VALUE_PAIR *received, *send, **vp;
-    char msg[4096];
-    char *rc_conf;
+    VALUE_PAIR *send, **vp;
+    char *rc_conf, *cp;
+    char lbuf[4096];
 
     rc_conf = RC_CONFIG_FILE;
     nas_port = 5060;
 
     acct = 0;
-    while ((ch = getopt(argc, argv, "af:p:")) != -1) {
+    server = 0;
+    while ((ch = getopt(argc, argv, "af:p:s")) != -1) {
         switch (ch) {
         case 'f':
             rc_conf = optarg;
@@ -67,6 +71,10 @@ main(int argc, char **argv)
             acct = 1;
             break;
 
+        case 's':
+            server = 1;
+            break;
+
         default:
             usage();
         }
@@ -74,7 +82,7 @@ main(int argc, char **argv)
     argc -= optind;
     argv += optind;
 
-    if (argc == 0)
+    if ((argc == 0 && server == 0) || (argc != 0 && server != 0))
         usage();
 
     if ((rh = rc_read_config(rc_conf)) == NULL) {
@@ -87,24 +95,84 @@ main(int argc, char **argv)
         exit(2);
     }
 
-    send = NULL;
-    vp = &send;
-    for (i = 0; i < argc; i++) {
-        if (rc_avpair_parse(rh, argv[i], vp) < 0) {
-            fprintf(stderr, "%s: can't parse AV pair\n", argv[i]);
-            exit(3);
+    if (server == 0) {
+        send = NULL;
+        vp = &send;
+        for (i = 0; i < argc; i++) {
+            if (rc_avpair_parse(rh, argv[i], vp) < 0) {
+                fprintf(stderr, "%s: can't parse AV pair\n", argv[i]);
+                exit(3);
+            }
+            vp = &send->next;
         }
-        vp = &send->next;
+        exit(process(rh, send, acct, nas_port));
     }
+    while (1 == 1) {
+        send = NULL;
+        vp = &send;
+        ecount = 0;
+        firstline = 1;
+        acct = 0;
+        do {
+            len = 0;
+            cp = fgetln(stdin, &len);
+            theend = 1;
+            if (cp != NULL && len > 0) {
+                if (firstline != 0) {
+                    if (len >= 4 && memcmp(cp, "ACCT", 4) == 0)
+                        acct = 1;
+                    firstline = 0;
+                    theend = 0;
+                    continue;
+                }
+                for (i = 0; i < len; i++) {
+                    if (!isspace(cp[i])) {
+                        theend = 0;
+                        break;
+                    }
+                }
+                if (theend == 0) {
+                    memcpy(lbuf, cp, len);
+                    lbuf[len] = '\0';
+                    if (rc_avpair_parse(rh, lbuf, vp) < 0) {
+                        fprintf(stderr, "%s: can't parse AV pair\n", lbuf);
+                        ecount++;
+                    } else {
+                        vp = &send->next;
+                    }
+                }
+            }
+        } while (theend == 0);
+        if (send != NULL && ecount == 0)
+            printf("%d\n\n", process(rh, send, acct, nas_port));
+        else
+            printf("%d\n\n", -1);
+        fflush(stdout);
+        if (send != NULL)
+            rc_avpair_free(send);
+        if (cp == NULL || len <= 0)
+            break;
+    }
+    exit(0);
+}
+
+int
+process(void *rh, VALUE_PAIR *send, int acct, int nas_port)
+{
+    VALUE_PAIR *received;
+    char msg[4096];
+    int i;
 
     received = NULL;
     if (acct == 0) {
         i = rc_auth(rh, nas_port, send, &received, msg);
-        if (received != NULL)
+        if (received != NULL) {
             printf("%s", rc_avpair_log(rh, received));
+            rc_avpair_free(received);
+        }
     } else {
         i = rc_acct(rh, nas_port, send);
     }
 
-    exit((i == OK_RC) ? 0 : 4);
+    return (i == OK_RC) ? 0 : 1;
 }
